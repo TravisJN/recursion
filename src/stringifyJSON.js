f9fc9c1ea52c7a71db51096e3757e8a509ecfd67
TravisJN <tdizzletrav@gmail.com>
2015-02-24 14:20:06 -0800

    completed stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,47 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  var str = "";
+
+	if (Array.isArray(obj)) {
+		//object is an array so add opening bracket
+		str += "[";
+		//loop through array, recursing over this function and adding the return value to str
+		for (var i = 0; i < obj.length; i++) {
+			str += stringifyJSON(obj[i]) + ",";
+		}
+		//this removes the last comma that was placed if indeed there was one
+		if (obj.length > 0) {
+			str = str.slice(0, str.length - 1);
+		}
+		str += "]";
+		return str;
+	} else if (typeof obj === 'object' && obj !== null) {
+		str += "{";
+		for (var key in obj) {
+			//if the key/value pair is a function or undefined skip it entirely
+			if (typeof obj[key] !== 'function' && obj[key] !== undefined) {
+				str += stringifyJSON(key);
+				str += ":";
+				str += stringifyJSON(obj[key]) + ",";
+			}
+			
+		}
+		//remove the last comma
+		if (str.length > 1) {
+			str = str.slice(0, str.length - 1);
+		}
+		str += "}";
+		return str;
+	}
+
+	//check each element and return a stringified version of it
+	if (obj === null){
+  		return 'null';
+  	}	else if (typeof obj === "string") {
+  		return '"' + obj + '"';
+  	} else {
+  		return obj.toString();
+  	}
+
 };
